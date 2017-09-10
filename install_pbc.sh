8147f5c18c3b417a90df46949e685fca9b5d7bcc
nikkolasg <nicolas.gailly@epfl.ch>
2017-09-10 18:33:40 +0200

    adding installer pbc

---
+++ install_pbc.sh
@@ -0,0 +1,77 @@
+#!/bin/bash 
+
+printh() {
+    echo "$(hostname)\t: $1"
+}
+
+printh "PBC prescript installer"
+#printh " -> copying library"
+#sudo cp "libbls384.so" "/usr/lib/"
+
+sudo apt-get install -y wget
+#STDLIB="libstd.deb"
+#if [ ! -f "$STDLIB" ]; then 
+    #wget "http://security.ubuntu.com/ubuntu/pool/main/g/gcc-4.8/libstdc++6_4.8.4-2ubuntu1~14.04.3_amd64.deb" -O "$STDLIB"
+#fi
+#dpkg -i "$STDLIB"
+
+### installing libgmp anyway
+sudo apt-get -y install libssl-dev
+sudo apt-get -y install libgmp-dev
+### doing symlinks for openssl weirdness ??
+OLD="$(pwd)"
+if [ ! -f "/usr/lib/x86_64-linux-gnu/libcrypto.so.1.1" ]; then
+    printh " -> linking libcrypto.so.1.1"
+    cd "/usr/lib/x86_64-linux-gnu"
+    sudo ln -s libcrypto.so libcrypto.so.1.1
+    cd "$OLD"
+fi
+
+LINK="https://s3-us-west-2.amazonaws.com/dfinity/crypto/bn/latest/bn-latest-amd64-linux-ubuntu16.04.tar.gz"
+TAR_NAME="bn-latest-amd64-linux-ubuntu16.04.tar.gz"
+TAR_LIB_PATH="bn-r20170708-2-amd64-linux-ubuntu16.04/lib/libbls384.so"
+SYS_LIB_PATH="/usr/lib/libbls384.so"
+
+extract() {
+    echo "[+] Extracting the library."
+    tar xvf "$TAR_NAME"
+}
+
+make_link() {
+    echo "[+] Creating symlink"
+    # simply do a symbolic link        
+    sudo ln -s "$(pwd)/$TAR_LIB_PATH" "$SYS_LIB_PATH"
+    sudo ldconfig
+}
+
+# check if library is already setup
+if [ -f "$SYS_LIB_PATH" ]; then
+    echo "[+] Library already installed. Exiting."
+    sudo ldconfig
+    exit 0
+fi
+
+# check if library is not extracted yet
+if [ -f "$TAR_NAME" ] && [ ! -f "$tAR_LIB_PATH"]; then
+    echo "[+] Library not extracted yet"
+    extract
+    make_link
+    exit 0
+fi 
+
+# check if library is already downloaded and extracted
+if [ -f "$TAR_LIB_PATH" ]; then
+    echo "[+] Library already downloaded."
+    make_link
+    exit 0
+fi
+
+
+echo "[+] Downloading and extracting the library..."
+# dl and extract the library
+wget "$LINK"
+extract
+make_link
+sudo ldconfig
+echo "[+] Bye !"
+exit 0
