1d71c2845ea060d874e10fddfe2da450e5d7d73b
nikkolasg <nicolas.gailly@epfl.ch>
2017-09-15 11:01:45 +0200

    added install for 14.04 travis + docker

---
+++ install_bn_travis.sh
@@ -0,0 +1,25 @@
+#!/bin/bash 
+
+set -x
+
+printh() {
+    echo "$(hostname)\t: $1"
+}
+
+printh "PBC prescript installer"
+sudo apt-get update
+sudo apt-get -y install libssl-dev \
+                        libgmp-dev \
+                        git \
+                        make \
+sudo git clone https://github.com/dfinity/bn /bn
+sudo wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key|sudo apt-key add - 
+echo "deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.8 main" | sudo tee /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get -y install llvm-3.8 clang-3.8 g++
+export CXX=clang++-3.8
+export PATH=$PATH:/usr/lib/llvm-3.8/bin/
+export LD_LIBRARY_PATH=/lib:/usr/lib:/usr/local/lib
+cd /bn
+make
+sudo make install
