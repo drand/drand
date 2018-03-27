c15dc4098e46c5ad277d797d5b7f44454ff0b435
nikkolasg <nicolas.gailly@epfl.ch>
2018-03-27 17:44:38 +0200

    script to compile protobuf def. with correct path

--- protobuf/compile_proto.sh
+++ protobuf/compile_proto.sh
@@ -5,10 +5,8 @@ echo "Compilation of protobufs definitions to go files"
 echo
 find . -type d -print | tail -n +2 | while read dir; 
 do
-    cd "$dir"
     echo " - compiling directory $dir"
-    protoc --proto_path="$curr" -I . *.proto --go_out=plugins=grpc:. 
-    cd "$curr"
+    protoc -I. $dir/*.proto --go_out=plugins=grpc:$GOPATH/src
 done
 echo
 echo "Done!"
