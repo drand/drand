24b12ad3f2268b79f26b49fbacb5790ab1b24b60
nikkolasg <nicolas.gailly@epfl.ch>
2018-04-22 13:34:52 +0200

    converter from protobuf to kyber

---
+++ protobuf/crypto/converter.go
@@ -0,0 +1,74 @@
+// converter implements some routines to go back and forth from a protobuf
+// point and scalar to a kyber Point and Scalar interface.
+package crypto
+
+import (
+	"errors"
+	fmt "fmt"
+
+	"github.com/dedis/kyber"
+	"github.com/dedis/kyber/suites"
+)
+
+type ProtobufPoint = Point
+type ProtobufScalar = Scalar
+
+func ProtobufToKyberPoint(p *ProtobufPoint) (kyber.Point, error) {
+	groupName, exists := GroupID_name[int32(p.GetGid())]
+	if !exists {
+		return nil, fmt.Errorf("oid %d unknown", p.GetGid())
+	}
+	group, err := suites.Find(groupName)
+	if err != nil {
+		return nil, fmt.Errorf("group name %s unknown", groupName)
+	}
+	point := group.Point()
+	return point, point.UnmarshalBinary(p.GetData())
+}
+
+func KyberToProtobufPoint(p kyber.Point) (*ProtobufPoint, error) {
+	desc, ok := p.(kyber.Groupable)
+	if !ok {
+		return nil, errors.New("given point is not self describing")
+	}
+	group := desc.Group()
+	gid, exists := GroupID_value[group.String()]
+	if !exists {
+		return nil, fmt.Errorf("group %s is not registered to the protobuf mapping", group.String())
+	}
+	buffer, err := p.MarshalBinary()
+	return &ProtobufPoint{
+		Gid:  GroupID(gid),
+		Data: buffer,
+	}, err
+}
+
+func ProtobufToKyberScalar(p *ProtobufScalar) (kyber.Scalar, error) {
+	groupName, exists := GroupID_name[int32(p.GetGid())]
+	if !exists {
+		return nil, fmt.Errorf("oid %d unknown")
+	}
+	group, err := suites.Find(groupName)
+	if err != nil {
+		return nil, fmt.Errorf("group name %s unknown", groupName)
+	}
+	scalar := group.Scalar()
+	return scalar, scalar.UnmarshalBinary(p.GetData())
+}
+
+func KyberToProtobufScalar(s kyber.Scalar) (*ProtobufScalar, error) {
+	desc, ok := s.(kyber.Groupable)
+	if !ok {
+		return nil, errors.New("given point is not self describing")
+	}
+	group := desc.Group()
+	gid, exists := GroupID_value[group.String()]
+	if !exists {
+		return nil, fmt.Errorf("group %s is not registered to the protobuf mapping", group.String())
+	}
+	buffer, err := s.MarshalBinary()
+	return &ProtobufScalar{
+		Gid:  GroupID(gid),
+		Data: buffer,
+	}, err
+}
