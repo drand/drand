eb5c33aedf4ca46a46756afda432d8f07cedf7e3
PizzaWhisperer <mathilde.raynal@epfl.ch>
2018-08-09 11:30:00 +0200

    Own separation domain for dist_key

--- protobuf/drand/client.pb.gw.go
+++ protobuf/drand/client.pb.gw.go
@@ -94,15 +94,6 @@ func request_Info_DistKey_0(ctx context.Context, marshaler runtime.Marshaler, cl
 
 }
 
-func request_Info_DistKey_1(ctx context.Context, marshaler runtime.Marshaler, client InfoClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq DistKeyRequest
-	var metadata runtime.ServerMetadata
-
-	msg, err := client.DistKey(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
-	return msg, metadata, err
-
-}
-
 // RegisterRandomnessHandlerFromEndpoint is same as RegisterRandomnessHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterRandomnessHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
@@ -314,46 +305,13 @@ func RegisterInfoHandlerClient(ctx context.Context, mux *runtime.ServeMux, clien
 
 	})
 
-	mux.Handle("GET", pattern_Info_DistKey_1, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
-		ctx, cancel := context.WithCancel(req.Context())
-		defer cancel()
-		if cn, ok := w.(http.CloseNotifier); ok {
-			go func(done <-chan struct{}, closed <-chan bool) {
-				select {
-				case <-done:
-				case <-closed:
-					cancel()
-				}
-			}(ctx.Done(), cn.CloseNotify())
-		}
-		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
-		rctx, err := runtime.AnnotateContext(ctx, mux, req)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-		resp, md, err := request_Info_DistKey_1(rctx, inboundMarshaler, client, req, pathParams)
-		ctx = runtime.NewServerMetadataContext(ctx, md)
-		if err != nil {
-			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
-			return
-		}
-
-		forward_Info_DistKey_1(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
-
-	})
-
 	return nil
 }
 
 var (
-	pattern_Info_DistKey_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"public"}, ""))
-
-	pattern_Info_DistKey_1 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0}, []string{"dist_key"}, ""))
+	pattern_Info_DistKey_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"info", "dist_key"}, ""))
 )
 
 var (
 	forward_Info_DistKey_0 = runtime.ForwardResponseMessage
-
-	forward_Info_DistKey_1 = runtime.ForwardResponseMessage
 )
