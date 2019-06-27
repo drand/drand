bb0ebde07ebac33df93eb5a55dee0113463a01f6
Ludovic Barman <ludovic.barman@protonmail.com>
2019-06-27 12:01:11 +0200

    Add DKG, but now fails due to invalid TLS certificates

--- test-integration/data/client-script.sh
+++ test-integration/data/client-script.sh
@@ -3,6 +3,14 @@
 IP_ADDR=`ip a | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b" | grep 172.33.0`
 IP_ADDR_PORT="${IP_ADDR}:${PORT}"
 
+SHARED_FOLDER="/data"
+PUBLIC_KEY_FILE="${SHARED_FOLDER}/${PORT}.public"
+GROUP_FILE="${SHARED_FOLDER}/group.toml"
+TLS_CERTS_FOLDER="${SHARED_FOLDER}/TLS_certificates_${PORT}/"
+TLS_KEY="${TLS_CERTS_FOLDER}/key.pem"
+TLS_CERT="${TLS_CERTS_FOLDER}/cert.pem"
+
+
 echo "My IP is ${IP_ADDR_PORT}"
 echo
 
@@ -10,15 +18,34 @@ echo
 echo "Generating key pair..."
 rm -rf /root/.drand/
 drand generate-keypair "${IP_ADDR_PORT}"
-echo "Here they are is:"
-ls /root/.drand/key
+cp /root/.drand/key/drand_id.public "${PUBLIC_KEY_FILE}"
+chmod ugo+rwx "${PUBLIC_KEY_FILE}"
+
 PUBLIC_KEY=`cat /root/.drand/key/drand_id.public`
-echo -e "${PUBLIC_KEY}\n" >> /data/group.toml
+echo -en "[[Nodes]]\n${PUBLIC_KEY}\n\n" >> "${GROUP_FILE}"
 echo
 
-# Wait for all containers to have done the same
-sleep 10
+# Generate the TLS certificates
+GOROOT=$(go env GOROOT)
+mkdir -p "${TLS_CERTS_FOLDER}"
+chmod ugo+rwx "${TLS_CERTS_FOLDER}"
+cd "${TLS_CERTS_FOLDER}"
+go run $GOROOT/src/crypto/tls/generate_cert.go --host $host --rsa-bits 1024 > /dev/null 2>& 1
 
-drand check-group /data/group.toml
+# Boot the drand deamon in background
+nohup drand --verbose 2 start --tls-cert "${TLS_CERT}" --tls-key "${TLS_KEY}" &
+
+# Wait for all containers to have done the same
+sleep 5
+
+if [[ "$LEADER" == 1 ]]; then
+    sleep 5
+    echo "We are the leader, running DKG"
+    drand share --leader "${GROUP_FILE}"
+else
+    drand share "${GROUP_FILE}"
+fi
+
+sleep 15
 
 echo "Done"
--- test-integration/test.sh
+++ test-integration/test.sh
@@ -12,8 +12,10 @@ cleanup () {
 trap 'cleanup ; printf "Tests have been killed via signal.\n"' HUP INT QUIT PIPE TERM
 
 # clear the previous group
-echo -en "[[Nodes]]\n\n" > "${GROUP_FILE}" 
-
+echo -en "Threshold = 3\nPeriod = \"1m0s\"\n\n" > "${GROUP_FILE}" 
+chmod ugo+rwx "${GROUP_FILE}" 
+rm -f data/*.public
+rm -rf data/TLS_certificates_*
 
 # build and run the composed services
 docker-compose -p "${DOCKER_COMPOSE_PROJECT_NAME}" -f "${DOCKER_COMPOSE_FILE}" up -d # add "--build" here
@@ -22,17 +24,18 @@ if [ $? -ne 0 ] ; then
   exit -1
 fi
 
-docker-compose -p "${DOCKER_COMPOSE_PROJECT_NAME}" -f "${DOCKER_COMPOSE_FILE}" logs
+docker-compose -p "${DOCKER_COMPOSE_PROJECT_NAME}" -f "${DOCKER_COMPOSE_FILE}" logs -f
+
+echo "Gonna test for web server API..."
+
+sleep 1000
 
 cleanup
 exit 0
 
 
 # wait for the test service to complete and grab the exit code
-TEST_EXIT_CODE=`docker wait ci_integration-tester_1`
-
-# output the logs for the test (for clarity)
-docker logs ci_integration-tester_1
+#TEST_EXIT_CODE=`docker wait ci_integration-tester_1`
 
 # inspect the output of the test and display respective message
 if [ -z ${TEST_EXIT_CODE+x} ] || [ "$TEST_EXIT_CODE" -ne 0 ] ; then
