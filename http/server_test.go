9c843618f35c22f8aa17dc4f52314db3d3617100
Will <will@cypherpunk.email>
2020-05-13 08:37:47 -0700

    client stability fixes (#304)
        Hopefully address #303 with some timeouts on startup
        warnings print more cleanly
        slack added to watch routine to make sure randomness is ready when polled
        client binary switched over to functional options

--- http/server_test.go
+++ http/server_test.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"net/http"
 	"testing"
+	"time"
 
 	"github.com/drand/drand/protobuf/drand"
 	"github.com/drand/drand/test/mock"
@@ -27,6 +28,7 @@ func TestHTTPRelay(t *testing.T) {
 	defer cancel()
 
 	client := drand.NewPublicClient(conn)
+	time.Sleep(100 * time.Millisecond)
 
 	handler, err := New(ctx, client, nil)
 	if err != nil {
@@ -40,6 +42,7 @@ func TestHTTPRelay(t *testing.T) {
 	server := http.Server{Handler: handler}
 	go server.Serve(listener)
 	defer server.Shutdown(ctx)
+	time.Sleep(100 * time.Millisecond)
 
 	// Test exported interfaces.
 	u := fmt.Sprintf("http://%s/public/1", listener.Addr().String())
