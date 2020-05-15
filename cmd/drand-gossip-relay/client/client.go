4b8884ac72aaaf1d26218a1cbb26caee72ac469a
Jakub Sztandera <kubuxu@protonmail.ch>
2020-05-15 12:03:40 +0200

    feat: relay randomness using libp2p PubSub (with Gossipsub Router) (#279)
    This PR implements randomness relay based on pubsub.
    I have removed randomness history fetching as it was incomplete.
    
    It connects to drand via gRPC for streaming API and rebroadcasts
    randomness via libp2p pubsub.
    
    `client` package implements a client for these pubsub messages.
    To test it out, run `drand-gossip-relay run --connect "$drandAPI"
    [--insecure | --cert ].
    
    Then note down libp2p printed peer info and run: `drand-gossip-relay
    --connect-to "$peerinfo"` to run a client.
    
    I've also set it up with separate `go.mod` file so the main `go.mod` is
    not polluted by libp2p dependencies.
    
    Signed-off-by: Jakub Sztandera <kubuxu@protocol.ai>

---
+++ cmd/drand-gossip-relay/client/client.go
@@ -0,0 +1,124 @@
+package client
+
+import (
+	"context"
+	"sync"
+
+	"github.com/drand/drand/cmd/drand-gossip-relay/lp2p"
+	"github.com/drand/drand/protobuf/drand"
+	"github.com/gogo/protobuf/proto"
+	logging "github.com/ipfs/go-log/v2"
+	pubsub "github.com/libp2p/go-libp2p-pubsub"
+	"golang.org/x/xerrors"
+)
+
+var (
+	log = logging.Logger("drand-client")
+)
+
+type Client struct {
+	cancel func()
+	latest uint64
+
+	subs struct {
+		sync.Mutex
+		M map[*int]chan drand.PublicRandResponse
+	}
+}
+
+// NewWtihPubsub creates a gossip randomness client.
+func NewWithPubsub(ps *pubsub.PubSub, networkName string) (*Client, error) {
+	t, err := ps.Join(lp2p.PubSubTopic(networkName))
+	if err != nil {
+		return nil, xerrors.Errorf("joining pubsub: %w", err)
+	}
+	s, err := t.Subscribe()
+	if err != nil {
+		return nil, xerrors.Errorf("subscribe: %w", err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	c := &Client{
+		cancel: cancel,
+	}
+	c.subs.M = make(map[*int]chan drand.PublicRandResponse)
+
+	go func() {
+		for {
+			msg, err := s.Next(ctx)
+			if ctx.Err() != nil {
+				c.subs.Lock()
+				for _, ch := range c.subs.M {
+					close(ch)
+				}
+				c.subs.M = make(map[*int]chan drand.PublicRandResponse)
+				c.subs.Unlock()
+				t.Close()
+				s.Cancel()
+				return
+			}
+			if err != nil {
+				log.Warnf("topic.Next error: %+v", err)
+				continue
+			}
+			var rand drand.PublicRandResponse
+			err = proto.Unmarshal(msg.Data, &rand)
+			if err != nil {
+				log.Warnf("unmarshaling randomness: %+v", err)
+				continue
+			}
+
+			// TODO: verification, need to pass drand network public key in
+
+			if c.latest >= rand.Round {
+				continue
+			}
+			c.latest = rand.Round
+
+			c.subs.Lock()
+			for _, ch := range c.subs.M {
+				select {
+				case ch <- rand:
+				default:
+					log.Warn("randomness notification dropped due to a full channel")
+				}
+			}
+			c.subs.Unlock()
+
+		}
+	}()
+
+	return c, nil
+}
+
+type UnsubFunc func()
+
+// Sub subscribes to notfications about new randomness.
+// Client instnace owns the channel after it is passed to Sub function,
+// thus the channel should not be closed by library user
+//
+// It is recommended to use a buffered channel. If the channel is full,
+// notification about randomness will be dropped.
+//
+// Notification channels will be closed when the client is Closed
+func (c *Client) Sub(ch chan drand.PublicRandResponse) UnsubFunc {
+	id := new(int)
+	c.subs.Lock()
+	c.subs.M[id] = ch
+	c.subs.Unlock()
+
+	return func() {
+		c.subs.Lock()
+		delete(c.subs.M, id)
+		close(ch)
+		c.subs.Unlock()
+	}
+}
+
+// Close stops Client, cancels PubSub subscription and closes the topic.
+func (c *Client) Close() error {
+	c.cancel()
+	return nil
+}
+
+// TODO: New for users without libp2p already running
