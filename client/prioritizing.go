5bd65f27be7c38dca87fe8c00c778a903b732452
Petar Maymounkov <petarm@gmail.com>
2020-05-20 11:32:46 -0700

    fix naming

--- client/prioritizing.go
+++ client/prioritizing.go
@@ -11,29 +11,29 @@ import (
 
 // NewPrioritizingClient is a client combinator that asks sub-clients
 // in succession until an answer is found.
-// Get requests are sourced from CDN sub-clients.
-// Watch requests are sourced from a gossip sub-client if available, otherwise
-// it is achieved as a long-poll from the prioritized CDN subclients.
-func NewPrioritizingClient(gossip Client, cdn []Client, groupHash []byte, group *key.Group, log log.Logger) (Client, error) {
-	return &prioritizingClient{gossip, cdn, groupHash, group, log}, nil
+// Get requests are sourced from get sub-clients.
+// Watch requests are sourced from a watch sub-client if available, otherwise
+// it is achieved as a long-poll from the prioritized get sub-clients.
+func NewPrioritizingClient(watchClient Client, getClient []Client, groupHash []byte, group *key.Group, log log.Logger) (Client, error) {
+	return &prioritizingClient{watchClient, getClient, groupHash, group, log}, nil
 }
 
 type prioritizingClient struct {
-	gossip    Client
-	cdn       []Client
-	groupHash []byte
-	group     *key.Group
-	log       log.Logger
+	watchClient Client
+	getClient   []Client
+	groupHash   []byte
+	group       *key.Group
+	log         log.Logger
 }
 
 // Get returns a the randomness at `round` or an error.
 func (p *prioritizingClient) Get(ctx context.Context, round uint64) (res Result, err error) {
-	for i, c := range p.cdn {
+	for i, c := range p.getClient {
 		res, err = c.Get(ctx, round)
 		if err == nil {
 			// previous clients failed. move them to end of priority.
 			if i > 0 {
-				p.cdn = append(p.cdn[i:], p.cdn[:i]...)
+				p.getClient = append(p.getClient[i:], p.getClient[:i]...)
 			}
 			return
 		}
@@ -50,7 +50,7 @@ func (p *prioritizingClient) learnGroup(ctx context.Context) error {
 	var group *key.Group
 	var err error
 
-	for _, c := range p.cdn {
+	for _, c := range p.getClient {
 		if hc, ok := c.(*httpClient); ok {
 			group, err = hc.FetchGroupInfo(p.groupHash)
 			if err == nil {
@@ -67,11 +67,11 @@ func (p *prioritizingClient) learnGroup(ctx context.Context) error {
 
 // Watch returns new randomness as it becomes available.
 func (p *prioritizingClient) Watch(ctx context.Context) <-chan Result {
-	// prefer the gossip channel for watches
-	if p.gossip != nil {
-		return p.gossip.Watch(ctx)
+	// prefer the watchClient channel for watches
+	if p.watchClient != nil {
+		return p.watchClient.Watch(ctx)
 	}
-	// otherwise, poll from the prioritized list of cdns
+	// otherwise, poll from the prioritized list of getClients
 	if p.group == nil {
 		if err := p.learnGroup(ctx); err != nil {
 			log.DefaultLogger.Warn("prioritizing_client", "failed to learn group", "err", err)
@@ -86,5 +86,5 @@ func (p *prioritizingClient) Watch(ctx context.Context) <-chan Result {
 // RoundAt will return the most recent round of randomness that will be available
 // at time for the current client.
 func (p *prioritizingClient) RoundAt(time time.Time) uint64 {
-	return p.cdn[0].RoundAt(time)
+	return p.getClient[0].RoundAt(time)
 }
