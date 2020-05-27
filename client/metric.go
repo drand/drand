669f3f3a2f533fc51b5b37768d9cef15299e98b0
Petar Maymounkov <petarm@gmail.com>
2020-05-27 13:46:38 -0700

    ok

--- client/metric.go
+++ client/metric.go
@@ -8,12 +8,48 @@ import (
 	"github.com/drand/drand/metrics"
 )
 
+func newHTTPHealthMetricClient(base Client, info *chain.Info) (Client, error) {
+	c := &httpHealthMetricClient{
+		Client:    base,
+		chainInfo: info,
+	}
+	go c.startObserve(context.Background())
+	return c, nil
+}
+
+type httpHealthMetricClient struct {
+	Client
+	chainInfo *chain.Info
+}
+
+// HTTPHeartbeatInterval is the duration between liveness heartbeats sent to an HTTP API.
+const HTTPHeartbeatInterval = 10 * time.Second
+
+func (c *httpHealthMetricClient) startObserve(ctx context.Context) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		}
+		time.Sleep(HTTPHeartbeatInterval)
+		result, err := c.Client.Get(ctx, 0)
+		if err != nil {
+			XXX
+			continue
+		}
+		// compute the latency metric
+		actual := time.Now().Unix()
+		expected := chain.TimeOfRound(c.chainInfo.Period, c.chainInfo.GenesisTime, result.Round())
+		// the labels of the gauge vec must already be set at the registerer level
+		metrics.XXXClientWatchLatency.Set(float64(expected - actual))
+	}
+}
+
 func newWatchLatencyMetricClient(base Client, info *chain.Info) (Client, error) {
 	c := &watchLatencyMetricClient{
 		Client:    base,
 		chainInfo: info,
 	}
-
 	go c.startObserve(context.Background())
 	return c, nil
 }
