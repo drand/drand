8a0010bfc64ee68f41c75da44df1f45665df97c3
Petar Maymounkov <petarm@gmail.com>
2020-06-02 11:29:14 -0700

    ok

--- cmd/client/testnet-observer/aws_ami_setup.sh
+++ cmd/client/testnet-observer/aws_ami_setup.sh
@@ -7,6 +7,7 @@ sudo yum update -y
 sudo yum install -y docker
 sudo service docker start
 sudo usermod -a -G docker ec2-user
+# restart terminal
 docker login
 docker pull drandorg/testnet-observer:1.0
 
@@ -14,6 +15,16 @@ docker pull drandorg/testnet-observer:1.0
 docker run -d \
         --restart always -p 42777:42777 -p 32111:32111 \
         -e DRAND_CLIENT_METRICS_ID=drand-testnet-observer-ohio \
+        drandorg/testnet-observer:1.0
+
+docker run -d \
+        --restart always -p 42777:42777 -p 32111:32111 \
+        -e DRAND_CLIENT_METRICS_ID=drand-testnet-observer-hongkong \
+        drandorg/testnet-observer:1.0
+
+docker run -d \
+        --restart always -p 42777:42777 -p 32111:32111 \
+        -e DRAND_CLIENT_METRICS_ID=drand-testnet-observer-stockholm \
         drandorg/testnet-observer:1.0
 
 # start telegraf
