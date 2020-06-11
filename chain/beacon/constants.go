17d4b7dea985278ea4ff4d59e08db3ab3ec69e4e
Will <willscott@gmail.com>
2020-06-10 19:00:28 -0700

    Revert "Fix chain cache DOS vuln (#533)"
    This reverts commit 41df34394f60b2cc867dc0bf9076deada9a1d890.

--- chain/beacon/constants.go
+++ chain/beacon/constants.go
@@ -5,10 +5,3 @@ import "time"
 // MaxSyncWaitTime sets how long we'll wait after a new connection to receive new beacons
 // from one peer
 var MaxSyncWaitTime = 2 * time.Second
-
-// MaxPartialperNode is the maximum number of partials the cache stores about
-// any node at any given time. This constant could be much lower, 3 for example
-// but when the network is catching up, it may happen that some nodes goes much
-// faster than other. In that case, multiple partials can be received from a
-// fast nodes and these are valid.
-const MaxPartialsPerNode = 100
