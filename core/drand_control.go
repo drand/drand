80c77299f5c6503e275fa01072caa205f40fb5f0
Will Scott <will@cypherpunk.email>
2020-06-24 10:55:12 -0700

    rebase of share override logic

--- core/drand_control.go
+++ core/drand_control.go
@@ -66,9 +66,7 @@ func (d *Drand) leaderRunSetup(newSetup func() (*setupManager, error)) (*key.Gro
 	d.state.Lock()
 	if d.manager != nil {
 		d.log.Info("reshare", "already_in_progress", "restart", "reshare")
-		d.state.Unlock()
 		d.manager.StopPreemptively()
-		return nil, errors.New("drand: setup dkg already in progress")
 	}
 	manager, err := newSetup()
 	if err != nil {
