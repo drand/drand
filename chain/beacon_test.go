737052d077d7f9301b10d59cccf26653ca9416f9
nikkolasg <nikkolasg@gmail.com>
2020-07-07 11:29:22 +0100

    remove test signature of beacon

--- chain/beacon_test.go
+++ chain/beacon_test.go
@@ -1,11 +1,9 @@
 package chain
 
 import (
-	"crypto/cipher"
 	"testing"
 
 	"github.com/drand/drand/key"
-	"github.com/drand/kyber"
 	"github.com/drand/kyber/util/random"
 )
 
@@ -23,37 +21,6 @@ func BenchmarkVerifyBeacon(b *testing.B) {
 			Round:       16,
 			Signature:   sig,
 		})
-		if err != nil {
-			panic(err)
-		}
-	}
-}
-
-type SchnorrScheme struct {
-	kyber.Group
-}
-
-func (s *SchnorrScheme) RandomStream() cipher.Stream {
-	return random.New()
-}
-
-func TestVerify(t *testing.T) {
-	secret := key.KeyGroup.Scalar().Pick(random.New())
-	public := key.KeyGroup.Point().Mul(secret, nil)
-	var round uint64 = 16
-	prevSig := []byte("My Sweet Previous Signature")
-	msg := Message(round, prevSig)
-	sig, _ := key.AuthScheme.Sign(secret, msg)
-	//scheme := schnorr.NewScheme(&SchnorrScheme{key.KeyGroup})
-	//sig, _ := scheme.Sign(secret, msg)
-	for i := 0; i < 100*50; i++ {
-		//err := scheme.Verify(public, msg, sig)
-		err := VerifyBeacon(public, &Beacon{
-			PreviousSig: prevSig,
-			Round:       16,
-			Signature:   sig,
-		})
-
 		if err != nil {
 			panic(err)
 		}
