d3f35586fe8604ba2f3c647179bf60d3f43a9a4b
Alan Shaw <alan@tableflip.io>
2020-07-17 17:03:05 +0100

    test: old verified result

--- client/verify_test.go
+++ client/verify_test.go
@@ -10,7 +10,7 @@ import (
 )
 
 func TestVerify(t *testing.T) {
-	info, results := mock.VerifiableResults(3)
+	info, results := mock.VerifiableResults(10)
 	mc := client.MockClient{Results: results, StrictRounds: true}
 	c, err := client.Wrap(
 		[]client.Client{client.MockClientWithInfo(info), &mc},
@@ -27,5 +27,27 @@ func TestVerify(t *testing.T) {
 	}
 	if res.Round() != results[1].Round() {
 		t.Fatal("expected to get result.", results[1].Round(), res.Round(), fmt.Sprintf("%v", c))
+	}
+}
+
+func TestVerifyWithOldVerifiedResult(t *testing.T) {
+	info, results := mock.VerifiableResults(5)
+	mc := client.MockClient{Results: results, StrictRounds: true}
+	c, err := client.Wrap(
+		[]client.Client{client.MockClientWithInfo(info), &mc},
+		client.WithChainInfo(info),
+		client.WithVerifiedResult(&results[0]),
+		client.WithFullChainVerification(),
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+	// should automatically load rounds 1, 2 and 3 to verify 4
+	res, err := c.Get(context.Background(), results[4].Round())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.Round() != results[4].Round() {
+		t.Fatal("expected to get result.", results[4].Round(), res.Round(), fmt.Sprintf("%v", c))
 	}
 }
