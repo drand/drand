f0f388779057da56fef3d566f88b62e04318913e
Kirk Baird <baird.k@outlook.com>
2020-08-13 15:42:29 +1000

    gofmt
    Signed-off-by: Kirk Baird <baird.k@outlook.com>

--- chain/time.go
+++ chain/time.go
@@ -23,7 +23,7 @@ func TimeOfRound(period time.Duration, genesis int64, round uint64) int64 {
 		return TimeOfRoundErrorValue
 	}
 
-	periodBits := math.Log2(float64(period.Seconds()) + 1) // require x >=1 in log2(x)
+	periodBits := math.Log2(float64(period.Seconds()) + 1)  // require x >=1 in log2(x)
 	if round >= (math.MaxUint64 >> (int(periodBits) + 2)) { // +1 for mul overflow, +1 for casting int64
 		return TimeOfRoundErrorValue
 	}
