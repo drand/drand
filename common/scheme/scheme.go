373b46c40b9494bdb9a067b51835072fbe1d609b
Emmanuel <emmanuelm41@gmail.com>
2021-10-13 12:55:13 -0300

    add docs to new added code

--- common/scheme/scheme.go
+++ common/scheme/scheme.go
@@ -5,9 +5,13 @@ import (
 	"os"
 )
 
+// DefaultSchemeID is the default scheme ID.
 const DefaultSchemeID = "pedersen-bls-chained"
+
+// UnchainedSchemeID is the scheme id used to set unchained randomness on beacons.
 const UnchainedSchemeID = "pedersen-bls-unchained"
 
+// Scheme is used to group a set of configurations related to the scheme beacons will use to generate randomness
 type Scheme struct {
 	ID              string
 	DecouplePrevSig bool
@@ -15,6 +19,8 @@ type Scheme struct {
 
 var schemes = []Scheme{{ID: DefaultSchemeID, DecouplePrevSig: false}, {ID: UnchainedSchemeID, DecouplePrevSig: true}}
 
+// GetSchemeByID allows the user to retrieve the scheme configuration looking by its ID. It will return a boolean which indicates
+// if the scheme was found or not.
 func GetSchemeByID(id string) (scheme Scheme, found bool) {
 	for _, t := range schemes {
 		if t.ID == id {
@@ -25,6 +31,9 @@ func GetSchemeByID(id string) (scheme Scheme, found bool) {
 	return Scheme{}, false
 }
 
+// GetSchemeByIDWithDefault allows the user to retrieve the scheme configuration looking by its ID. It will return a boolean which indicates
+// if the scheme was foound or not. In addition to it, if the received ID is an empty string,
+// it will return the default defined scheme
 func GetSchemeByIDWithDefault(id string) (scheme Scheme, err error) {
 	if id == "" {
 		id = DefaultSchemeID
@@ -38,6 +47,7 @@ func GetSchemeByIDWithDefault(id string) (scheme Scheme, err error) {
 	return sch, nil
 }
 
+// ListSchemes will return a slice of valid scheme ids
 func ListSchemes() (schemeIDs []string) {
 	for _, t := range schemes {
 		schemeIDs = append(schemeIDs, t.ID)
@@ -46,6 +56,9 @@ func ListSchemes() (schemeIDs []string) {
 	return schemeIDs
 }
 
+// ReadSchemeByEnv allows the user to retrieve the scheme configuration looking by the ID set on an
+// environmental variable. It will return a boolean which indicates if the scheme was found or not.
+// If the env var is an empty string, it will use the default scheme ID.
 func ReadSchemeByEnv() (Scheme, bool) {
 	id := os.Getenv("SCHEME_ID")
 	if id == "" {
@@ -55,6 +68,8 @@ func ReadSchemeByEnv() (Scheme, bool) {
 	return GetSchemeByID(id)
 }
 
+// GetSchemeFromEnv allows the user to retrieve the scheme configuration looking by the ID set on an
+// environmental variable. If the scheme is not found, function will panic.
 func GetSchemeFromEnv() Scheme {
 	sch, ok := ReadSchemeByEnv()
 	if !ok {
