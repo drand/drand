d1d88b6799c2740b2c293e6c5456943966fcb4c8
nikkolasg <nikkolasg@gmail.com>
2021-11-19 18:33:42 +0000

    lint action simplified

--- protobuf/gen.go
+++ protobuf/gen.go
@@ -1,6 +1,7 @@
 /*
 Package protobuf contains wire definitions of messages passed between drand nodes.
 */
+// nolint: lll
 //go:generate protoc -I=. --go_out=. --go_opt=paths=source_relative common/common.proto
 //go:generate protoc -I=. --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false,paths=source_relative:. drand/api.proto drand/common.proto drand/control.proto drand/protocol.proto
 //go:generate protoc -I=. --go_out=. --go_opt=paths=source_relative crypto/dkg/dkg.proto
