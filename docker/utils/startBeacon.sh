e35b155bc2cf9e49f195e9145a5687ba0ffd0093
Ezequiel Raynaudo <raynaudo.ee@gmail.com>
2021-11-24 19:46:31 -0300

    Minor fix

--- docker/utils/startBeacon.sh
+++ docker/utils/startBeacon.sh
@@ -4,7 +4,7 @@
 # Start leader
 nohup docker exec -u drand drand_3 /bin/sh -c 'drand share --leader --nodes 4 --threshold 3 --period "5s"' &
 
-sleep 3s
+sleep 3
 
 # Start the rest of the nodes
 nohup docker exec -u drand drand_2 /bin/sh -c 'drand share --connect drand_3:8380 --tls-disable' &
@@ -12,7 +12,7 @@ nohup docker exec -u drand drand_1 /bin/sh -c 'drand share --connect drand_3:838
 nohup docker exec -u drand drand_0 /bin/sh -c 'drand share --connect drand_3:8380 --tls-disable' &
 
 
-sleep 10s
+sleep 10
 
 ## Test beacon
 ## Generate keys
@@ -24,7 +24,7 @@ nohup docker exec -u drand drand_3 /bin/sh -c 'drand generate-keypair --tls-disa
 # Start leader
 nohup docker exec -u drand drand_3 /bin/sh -c 'drand share --leader --nodes 4 --threshold 3 --period "60s" --id test_beacon' &
 
-sleep 3s
+sleep 3
 
 # Start the rest of the nodes
 nohup docker exec -u drand drand_2 /bin/sh -c 'drand share --connect drand_3:8380 --tls-disable --id test_beacon' &
