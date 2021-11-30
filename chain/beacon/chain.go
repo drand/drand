98c5115251e239ba76f342917bb89ad81eeb9668
nikkolasg <nikkolasg@gmail.com>
2021-11-30 21:33:16 +0000

    log for full queue for sync beacon

--- chain/beacon/chain.go
+++ chain/beacon/chain.go
@@ -204,6 +204,7 @@ func (c *chainStore) tryAppend(last, newB *chain.Beacon) bool {
 	// only send if it's not full already
 	case c.catchupBeacons <- newB:
 	default:
+		c.l.Debug("chain_store", "catchup", "channel", "full")
 	}
 	return true
 }
