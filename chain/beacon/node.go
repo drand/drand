98d7e9a7c78cef4238a33670c7014f89909d9674
nikkolasg <nikkolasg@gmail.com>
2021-12-07 20:52:12 +0000

    bruteforce catchup validation

--- chain/beacon/node.go
+++ chain/beacon/node.go
@@ -218,6 +218,15 @@ func (h *Handler) run(startTime int64) {
 	var current roundInfo
 	for {
 		select {
+		case <-h.conf.Clock.After(h.conf.Group.CatchupPeriod):
+			lastBeacon, err := h.chain.Last()
+			if err != nil {
+				h.l.Error("beacon_loop", "loading_last", "err", err)
+				break
+			}
+			if lastBeacon.Round+1 < current.round {
+				go h.broadcastNextPartial(current, lastBeacon)
+			}
 		case current = <-chanTick:
 			lastBeacon, err := h.chain.Last()
 			if err != nil {
@@ -239,25 +248,6 @@ func (h *Handler) run(startTime int64) {
 				// done. Not critical but leads to faster network recovery.
 				h.l.Debug("beacon_loop", "run_sync_catchup", "last_is", lastBeacon, "should_be", current.round)
 				go h.chain.RunSync(context.Background(), current.round, nil)
-			}
-		case b := <-h.chain.AppendedBeaconNoSync():
-			h.l.Debug("beacon_loop", "catchupmode", "last_is", b.Round, "current", current.round, "catchup_launch", b.Round < current.round)
-			if b.Round < current.round {
-				// When network is down, all alive nodes will broadcast their
-				// signatures periodically with the same period. As soon as one
-				// new beacon is created,i.e. network is up again, this channel
-				// will be triggered and we enter fast mode here.
-				// Since that last node is late, nodes must now hurry up to do
-				// the next beacons in time -> we run the next beacon now
-				// already. If that next beacon is created soon after, this
-				// channel will trigger again etc until we arrive at the correct
-				// round.
-				go func(c roundInfo, latest *chain.Beacon) {
-					h.l.Debug("beacon_loop", "catchupmode", "last_is", latest.Round, "seep_for", h.conf.Group.CatchupPeriod)
-					h.conf.Clock.Sleep(h.conf.Group.CatchupPeriod)
-					h.l.Debug("beacon_loop", "catchupmode", "last_is", latest.Round, "broadcast")
-					h.broadcastNextPartial(c, latest)
-				}(current, b)
 			}
 		case <-h.close:
 			h.l.Debug("beacon_loop", "finished")
