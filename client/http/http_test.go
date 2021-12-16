57089ccf3f2d728f2696fd94ce16552b7a3aef06
Ezequiel Raynaudo <raynaudo.ee@gmail.com>
2021-12-16 10:41:05 -0300

    Fix test attempt

--- client/http/http_test.go
+++ client/http/http_test.go
@@ -120,7 +120,7 @@ func TestHTTPWatch(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 2*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	result := httpClient.Watch(ctx)
 	first, ok := <-result
