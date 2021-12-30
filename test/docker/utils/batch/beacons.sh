e26d09ea21451dfa6269ceffd3d29309155329a6
Emmanuel <emmanuelm41@gmail.com>
2021-12-30 12:14:13 -0300

    make scripts simpler (#890)

--- test/docker/utils/batch/beacons.sh
+++ test/docker/utils/batch/beacons.sh
@@ -3,37 +3,41 @@
 NODES=5
 BEACONS=20
 PERIOD_STEP=5
+LEADER_NODE=3
+THRESHOLD=4
 
-nohup docker exec -u drand drand_0 /bin/sh -c "drand share --leader --nodes 5 --threshold 4 --period 2s" &
-sleep 1
-nohup docker exec -u drand drand_2 /bin/sh -c "drand share --connect drand_0:8080 --tls-disable" &
-sleep 1
-nohup docker exec -u drand drand_1 /bin/sh -c "drand share --connect drand_0:8080 --tls-disable" &
-sleep 1
-nohup docker exec -u drand drand_3 /bin/sh -c "drand share --connect drand_0:8080 --tls-disable" &
-sleep 1
-nohup docker exec -u drand drand_4 /bin/sh -c "drand share --connect drand_0:8080 --tls-disable" &
-sleep 1
 
-for (( beacon = 1; beacon <= BEACONS; beacon++ )); do
+for (( beacon = 0; beacon <= BEACONS; beacon++ )); do
 
   PERIOD=$((${beacon}*PERIOD_STEP))
-  docker exec -u drand drand_0 /bin/sh -c "drand generate-keypair --tls-disable --id beacon_${PERIOD}s drand_0:8080"
-  docker exec -u drand drand_1 /bin/sh -c "drand generate-keypair --tls-disable --id beacon_${PERIOD}s drand_1:8180"
-  docker exec -u drand drand_2 /bin/sh -c "drand generate-keypair --tls-disable --id beacon_${PERIOD}s drand_2:8280"
-  docker exec -u drand drand_3 /bin/sh -c "drand generate-keypair --tls-disable --id beacon_${PERIOD}s drand_3:8380"
-  docker exec -u drand drand_4 /bin/sh -c "drand generate-keypair --tls-disable --id beacon_${PERIOD}s drand_4:8480"
 
+  for (( node = 0; node < NODES; node++ )); do
 
-  nohup docker exec -u drand drand_0 /bin/sh -c "drand share --leader --nodes 5 --threshold 4 --period ${PERIOD}s --id beacon_${PERIOD}s --scheme pedersen-bls-unchained" &
-  sleep 1
-  nohup docker exec -u drand drand_2 /bin/sh -c "drand share --connect drand_0:8080 --tls-disable --id beacon_${PERIOD}s" &
-  sleep 1
-  nohup docker exec -u drand drand_1 /bin/sh -c "drand share --connect drand_0:8080 --tls-disable --id beacon_${PERIOD}s" &
-  sleep 1
-  nohup docker exec -u drand drand_3 /bin/sh -c "drand share --connect drand_0:8080 --tls-disable --id beacon_${PERIOD}s" &
-  sleep 1
-  nohup docker exec -u drand drand_4 /bin/sh -c "drand share --connect drand_0:8080 --tls-disable --id beacon_${PERIOD}s" &
-  sleep 1
+    if [ $beacon -eq 0 ]; then
+      if [ $node -eq 0 ]; then
+        nohup docker exec -u drand drand_${LEADER_NODE} /bin/sh -c "drand share --leader --nodes ${NODES} --threshold ${THRESHOLD} --period 2s" &
+        sleep 1
+      fi
 
+      if [ $node -ne $LEADER_NODE ]; then
+        nohup docker exec -u drand drand_${node} /bin/sh -c "drand share --connect drand_${LEADER_NODE}:8${LEADER_NODE}80 --tls-disable" &
+        sleep 1
+      fi
+
+      continue
+    fi
+
+    if [ $node -eq 0 ]; then
+      docker exec -u drand drand_${LEADER_NODE} /bin/sh -c "drand generate-keypair --tls-disable --id beacon_${PERIOD}s drand_${LEADER_NODE}:8${LEADER_NODE}80"
+      nohup docker exec -u drand drand_${LEADER_NODE} /bin/sh -c "drand share --leader --nodes ${NODES} --threshold ${THRESHOLD} --period ${PERIOD}s --id beacon_${PERIOD}s --scheme pedersen-bls-unchained" &
+      sleep 1
+    fi
+
+    if [ $node -ne $LEADER_NODE ]; then
+      docker exec -u drand drand_${node} /bin/sh -c "drand generate-keypair --tls-disable --id beacon_${PERIOD}s drand_${node}:8${node}80"
+      nohup docker exec -u drand drand_${node} /bin/sh -c "drand share --connect drand_${LEADER_NODE}:8${LEADER_NODE}80 --tls-disable --id beacon_${PERIOD}s" &
+      sleep 1
+    fi
+
+  done
 done
