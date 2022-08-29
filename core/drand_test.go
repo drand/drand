0bc7098574cd4ba10fa5cd709d5e156e85092785
Yolan Romailler <yolan@protocol.ai>
2022-08-29 10:59:31 +0200

    Lint

--- core/drand_test.go
+++ core/drand_test.go
@@ -13,7 +13,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-  "github.com/weaveworks/common/fs"
+	"github.com/weaveworks/common/fs"
 
 	"github.com/drand/drand/chain"
 	"github.com/drand/drand/common/scheme"
