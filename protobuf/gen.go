2311033bc1e9c6c2a7a2e50dc75251fa16c8dc40
Bexanderthebex <melbex11@gmail.com>
2022-10-26 05:52:22 +0800

    protobuf: use correct protoc version

--- protobuf/gen.go
+++ protobuf/gen.go
@@ -1,7 +1,7 @@
 /*
 Package protobuf contains wire definitions of messages passed between drand nodes.
 */
-//go:generate protoc -I=. --go_out=. --go_opt=paths=source_relative common/common.proto --experimental_allow_proto3_optional
+//go:generate protoc -I=. --go_out=. --go_opt=paths=source_relative common/common.proto
 //go:generate protoc -I=. --go_out=. --go_opt=paths=source_relative --go-grpc_out=require_unimplemented_servers=false,paths=source_relative:. drand/api.proto drand/common.proto drand/control.proto drand/protocol.proto
 //go:generate protoc -I=. --go_out=. --go_opt=paths=source_relative crypto/dkg/dkg.proto
 package protobuf
