9b09cdea49b0881edcf85753b140e3d22d63516d
William Kennedy <bill@ardanlabs.com>
2022-11-07 18:44:31 -0500

    uppercasing format

--- chain/pg/sql/schema.sql
+++ chain/pg/sql/schema.sql
@@ -13,7 +13,7 @@ CREATE TYPE drand_round_offset AS
 CREATE FUNCTION drand_tablesize(tableName char) RETURNS bigint AS $$
 DECLARE ret bigint;
 BEGIN
-    EXECUTE (format('SELECT COUNT(*) FROM %I', tableName)) INTO ret;
+    EXECUTE (FORMAT('SELECT COUNT(*) FROM %I', tableName)) INTO ret;
     RETURN ret;
 END;
 $$ LANGUAGE plpgsql;
@@ -22,13 +22,13 @@ $$ LANGUAGE plpgsql;
 -- Description: Create function drand_insertround
 CREATE FUNCTION drand_insertround(tableName char, round bigint, signature bytea, previous_sig bytea) RETURNS VOID AS $$
 BEGIN
-    EXECUTE format('CREATE TABLE IF NOT EXISTS %I (
+    EXECUTE FORMAT('CREATE TABLE IF NOT EXISTS %I (
         round        BIGINT NOT NULL CONSTRAINT %1$I_pk PRIMARY KEY,
         signature    BYTEA  NOT NULL,
         previous_sig BYTEA  NOT NULL
     )', tableName);
 
-    EXECUTE format('INSERT INTO %I
+    EXECUTE FORMAT('INSERT INTO %I
         (round, signature, previous_sig)
     VALUES
         (%L, %L, %L)
@@ -41,7 +41,7 @@ $$ LANGUAGE plpgsql;
 CREATE OR REPLACE FUNCTION drand_getlastround(tableName char) RETURNS drand_round AS $$
 DECLARE ret drand_round;
 BEGIN
-    EXECUTE (format('SELECT round, signature, previous_sig FROM %I
+    EXECUTE (FORMAT('SELECT round, signature, previous_sig FROM %I
                     ORDER BY round DESC
                     LIMIT 1',
                     tableName)) INTO ret;
@@ -54,7 +54,7 @@ $$ LANGUAGE plpgsql;
 CREATE FUNCTION drand_getround(tableName char, round bigint) RETURNS drand_round AS $$
 DECLARE ret drand_round;
 BEGIN
-    EXECUTE (format('SELECT round, signature, previous_sig FROM %I
+    EXECUTE (FORMAT('SELECT round, signature, previous_sig FROM %I
                     WHERE round=%L
                     LIMIT 1',
                     tableName, round)) INTO ret;
@@ -66,7 +66,7 @@ $$ LANGUAGE plpgsql;
 -- Description: Create function drand_deleteround
 CREATE FUNCTION drand_deleteround(tableName char, round bigint) RETURNS VOID AS $$
 BEGIN
-    EXECUTE (format('SELECT round, signature, previous_sig FROM %I
+    EXECUTE (FORMAT('SELECT round, signature, previous_sig FROM %I
                     WHERE round=%L
                     LIMIT 1',
                     tableName, round));
@@ -78,7 +78,7 @@ $$ LANGUAGE plpgsql;
 CREATE FUNCTION drand_getroundposition(tableName char, round_num int) RETURNS drand_round_offset AS $$
 DECLARE ret drand_round_offset;
 BEGIN
-    EXECUTE (format('SELECT round_offset FROM (
+    EXECUTE (FORMAT('SELECT round_offset FROM (
             SELECT round, row_number() OVER(ORDER BY round ASC) AS round_offset
             FROM %I
             ORDER BY round ASC
@@ -93,7 +93,7 @@ $$ LANGUAGE plpgsql;
 CREATE FUNCTION drand_getfirstround(tableName char) RETURNS drand_round AS $$
 DECLARE ret drand_round;
 BEGIN
-    EXECUTE (format('SELECT round, signature, previous_sig FROM %I
+    EXECUTE (FORMAT('SELECT round, signature, previous_sig FROM %I
                     ORDER BY round ASC
                     LIMIT 1',
                     tableName)) INTO ret;
@@ -106,7 +106,7 @@ $$ LANGUAGE plpgsql;
 CREATE FUNCTION drand_getoffsetround(tableName char, r_offset bigint) RETURNS drand_round AS $$
 DECLARE ret drand_round;
 BEGIN
-    EXECUTE (format('SELECT round, signature, previous_sig FROM %I
+    EXECUTE (FORMAT('SELECT round, signature, previous_sig FROM %I
 					ORDER BY round ASC OFFSET %L LIMIT 1',
                      tableName, r_offset)) INTO ret;
     RETURN ret;
