2e395b87c4cd9dcd9ff70c5a04894df7479ed729
Yolan Romailler <yolan@protocol.ai>
2023-02-01 12:09:06 +0100

    patching minor issue

--- common/version.go
+++ common/version.go
@@ -47,6 +47,8 @@ func (v Version) IsCompatible(verRcv Version) bool {
 	// Hardcoded the latest potential breakage of network packets.
 	// Since v1.4.0 we are now using GRPC deprecation warnings to handle network packet changes.
 	switch {
+	case v.Major == verRcv.Major && v.Minor == verRcv.Minor:
+		return true
 	case verRcv.Major == 1 && verRcv.Minor >= 4:
 		return true
 	case v.Major == 2 && verRcv.Major == 1 && verRcv.Minor >= 5:
