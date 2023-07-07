2c0cea4890f1212c4c410f33aa3058edbf16e4b1
PM <3749956+CluEleSsUK@users.noreply.github.com>
2023-07-07 11:02:45 +0200

    DKG refactor: use existing keys to generate proposal (#1253)
    * use existing keys when generating proposal files
    * fixed an encoding test
    * copy instead of pointer to defeat the dreaded race detector
    * rebase + errors.Is

--- internal/util/func_utils.go
+++ internal/util/func_utils.go
@@ -1,5 +1,7 @@
 package util
 
+import "errors"
+
 // Concat combines multiple arrays of the same type into a single array
 func Concat[T any](arrs ...[]T) []T {
 	var out []T
@@ -7,6 +9,25 @@ func Concat[T any](arrs ...[]T) []T {
 		out = append(out, arr...)
 	}
 	return out
+}
+
+// Cont is contains, but the util package already contains another func just for `Participant`
+func Cont[T comparable](haystack []T, needle T) bool {
+	for _, v := range haystack {
+		if v == needle {
+			return true
+		}
+	}
+	return false
+}
+
+func First[T any](haystack []T, predicate func(T) bool) (*T, error) {
+	for _, value := range haystack {
+		if predicate(value) {
+			return &value, nil
+		}
+	}
+	return nil, errors.New("item not found in array")
 }
 
 func Filter[T any](arr []T, predicate func(T) bool) []T {
