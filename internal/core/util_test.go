22a360bb31b36b783d62e1fe4453fa724d24d2a2
Patrick McClurg <patrick.mcclurg@protocol.ai>
2024-02-16 16:00:28 +0100

    linter

--- internal/core/util_test.go
+++ internal/core/util_test.go
@@ -491,8 +491,7 @@ func (d *DrandTestScenario) RunReshare(
 	return d.RunReshareWithHooks(t, remainingNodes, joiningNodes, d.thr, lifecycleHooks{})
 }
 
-//nolint:funlen
-func (d *DrandTestScenario) RunReshareWithHooks(t *testing.T, remainingNodes []*MockNode, joiningNodes []*MockNode, threshold int, hooks lifecycleHooks) (*key.Group, error) {
+func (d *DrandTestScenario) RunReshareWithHooks(t *testing.T, remainingNodes, joiningNodes []*MockNode, threshold int, hooks lifecycleHooks) (*key.Group, error) {
 	if len(remainingNodes) == 0 {
 		return nil, errors.New("cannot run a DKG with 0 nodes in the drand test scenario")
 	}
