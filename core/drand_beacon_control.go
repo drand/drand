9477edfef97159d402b136a38dcf5faab432d8a7
Patrick McClurg <patrick.mcclurg@protocol.ai>
2024-03-27 15:27:40 +0100

    check group responses on change and not just after timeout

--- core/drand_beacon_control.go
+++ core/drand_beacon_control.go
@@ -1064,6 +1064,10 @@ func (bp *BeaconProcess) pushDKGInfo(outgoing, incoming []*key.Node, previousThr
 				"previousThreshold", previousThreshold,
 				"newThreshold", newThreshold,
 			)
+			if previousThreshold <= 0 && newThreshold <= 0 {
+				bp.log.Infow("", "push_dkg", "sending_group", "status", "enough succeeded", "missed", total)
+				return nil
+			}
 		case <-bp.opts.clock.After(time.Minute):
 			if previousThreshold <= 0 && newThreshold <= 0 {
 				bp.log.Infow("", "push_dkg", "sending_group", "status", "enough succeeded", "missed", total)
