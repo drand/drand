365ab9e0669c9cbc4ae57550764f118796a566f2
alienx5499 <p2005p5499p@gmail.com>
2025-12-10 23:17:30 +0530

    Reset logger node id during resharing

--- internal/core/drand_beacon.go
+++ internal/core/drand_beacon.go
@@ -55,6 +55,8 @@ type BeaconProcess struct {
 
 	// general logger
 	log dlog.Logger
+	// base logger without node index (used to recreate logger with new index after resharing)
+	baseLog dlog.Logger
 
 	// global state lock
 	state  sync.RWMutex
@@ -92,6 +94,7 @@ func NewBeaconProcess(ctx context.Context,
 		beaconID:      common.GetCanonicalBeaconID(beaconID),
 		store:         store,
 		log:           log,
+		baseLog:       log, // Store base logger (without node index)
 		priv:          priv,
 		version:       common.GetAppVersion(),
 		opts:          opts,
@@ -148,7 +151,7 @@ func (bp *BeaconProcess) Load(ctx context.Context) error {
 	}
 	bp.state.Lock()
 	bp.index = int(thisBeacon.Index)
-	bp.log = bp.log.Named(fmt.Sprint(bp.index))
+	bp.log = bp.baseLog.Named(fmt.Sprint(bp.index))
 	bp.state.Unlock()
 
 	bp.log.Debugw("", "serving", bp.priv.Public.Address())
@@ -267,6 +270,13 @@ func (bp *BeaconProcess) storeDKGOutput(ctx context.Context, group *key.Group, s
 	bp.group = group
 	bp.share = share
 	bp.chainHash = public.NewChainInfo(bp.group).Hash()
+
+	// Update node index and logger after group change (e.g., during resharing)
+	thisBeacon := bp.group.Find(bp.priv.Public)
+	if thisBeacon != nil {
+		bp.index = int(thisBeacon.Index)
+		bp.log = bp.baseLog.Named(fmt.Sprint(bp.index))
+	}
 
 	err := bp.store.SaveGroup(group)
 	if err != nil {
